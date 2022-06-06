6e0020632fcc51c2cbeb617be8bedd84fd5b75d3
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-06-06 22:16:18 +0300

    init commit with hello wolrld project

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World 1!" << std::endl;
+    std::cout << "Hello, World 2!" << std::endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "row number: " << i << endl;
+
+    }
+    
+    
+}
