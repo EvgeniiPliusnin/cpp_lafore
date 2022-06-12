49593b1869022c6a085325a1f257e858babfe5df
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-06-12 23:46:33 +0300

    complited examples from chapter 2

--- chapter2/width1.cpp
+++ chapter2/width1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
     long pop1 = 4789426, pop2 = 274124, pop3 = 976;
-    cout << setw(10) << "City " << setw(12) << "population "<< endl
+    cout << setw(10) << "City " << setw(12) << "population"<< endl
     << setw(10) << "Moscow " << setw(12) << pop1 << endl
     << setw(10) << "Kirov " << setw(12) << pop2 << endl
     << setw(10) << "Ygrumovka " << setw(12) <<pop3 << endl;
